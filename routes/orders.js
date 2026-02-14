826e2b2cd5de434b2e96b5f83bea3a243a23236c
Sneha-datla <datla.sneha1@gmail.com>
2026-02-14 13:31:09 +0530

    changes made

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,60 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
+
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+router.post("/create-razorpay-order", async (req, res) => {
+  try {
+    const { amount } = req.body;
+
+    const order = await razorpay.orders.create({
+      amount,
+      currency: "INR",
+      receipt: "receipt_" + Date.now(),
+    });
+
+    res.json(order);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to create order" });
+  }
+});
+
+
+router.post("/verify-razorpay-payment", async (req, res) => {
+  try {
+    const {
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature
+    } = req.body;
+
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+    const expectedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+      .update(body)
+      .digest("hex");
+
+    if (expectedSignature === razorpay_signature) {
+      res.json({ success: true });
+    } else {
+      res.json({ success: false });
+    }
+
+  } catch (err) {
+    res.status(500).json({ error: "Verification failed" });
+  }
+});
+
+
 
 /* =====================================
    1️⃣ CREATE ORDER (Place Order)
